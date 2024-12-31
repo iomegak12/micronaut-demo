f83db6752f5a06dfd1273e55442b3efe5021e04e
iomegak12 <jd.ramkumar@gmail.com>
2024-12-31 09:38:10 +0530

    final changes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM maven:3.9.9-eclipse-temurin-21-jammy as build
+FROM maven:3.9.9-eclipse-temurin-21-jammy AS build
 
 WORKDIR /app
 
--- src/main/java/com/intsol/Application.java
+++ src/main/java/com/intsol/Application.java
@@ -3,19 +3,40 @@ package com.intsol;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.*;
+import jakarta.inject.Singleton;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import io.micronaut.context.annotation.Value;
 
-@OpenAPIDefinition(
-    info = @Info(
-        title = "IntSol Customer Services", 
-        version = "1.0.0", 
-        description = "Simple POC to Demonstrate how Micronaut Works", 
-        license = @License(name = "MIT", url = "https://license.intsol.in"), 
-        contact = @Contact(url = "https://github.com/iomegak12/", 
-        name = "Ramkumar J Dhamodharan", 
-        email = "jd.ramkumar@gmail.com")))
+import io.micronaut.context.event.ApplicationEventListener;
+import io.micronaut.context.event.StartupEvent;
+
+@OpenAPIDefinition(info = @Info(title = "IntSol Customer Services", version = "1.0.0", description = "Simple POC to Demonstrate how Micronaut Works", license = @License(name = "MIT", url = "https://license.intsol.in"), contact = @Contact(url = "https://github.com/iomegak12/", name = "Ramkumar J Dhamodharan", email = "jd.ramkumar@gmail.com")))
 public class Application {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Singleton
+    static class OnStartupEventListener implements ApplicationEventListener<StartupEvent> {
+
+        @Value("${datasources.default.url}")
+        private String datasourcesDefaultUrl;
+
+        @Override
+        public void onApplicationEvent(StartupEvent event) {
+            log.warn("Database URL is : " + datasourcesDefaultUrl);
+        }
+
+        public String getdatasourcesDefaultUrl() {
+            return datasourcesDefaultUrl;
+        }
+
+        public void setdatasourcesDefaultUrl(String datasourcesDefaultUrl) {
+            this.datasourcesDefaultUrl = datasourcesDefaultUrl;
+        }
     }
 }
